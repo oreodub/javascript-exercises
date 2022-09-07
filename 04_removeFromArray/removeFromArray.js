528fb58a221b221026a786630a1e39b91f7f61a3
William Suh <william.junil.suh@gmail.com>
2022-09-07 16:44:16 +0900

    oops!

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...values) {
+    for (let i = 0; i < values.length; i++) {
+        let index = arr.indexOf(values[i]);
+        if (index > -1) {
+            arr.splice(index, 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
